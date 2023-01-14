1eac0530dfb06526cb2c7df727767cb8caf2bb3b
12626538 <57176042+12626538@users.noreply.github.com>
2023-01-14 13:20:51 +0100

    added documentation, cleaned up

--- models/bfw.py
+++ models/bfw.py
@@ -1,39 +1,74 @@
 import os
-from typing import Union,Optional
+from typing import Optional
 
-import numpy as np
 import pandas as pd
 
 from skimage import io
-from sklearn.model_selection import KFold
 
 import torch
-from torch.utils.data import Dataset, DataLoader, ConcatDataset
+from torch.utils.data import Dataset
 from torchvision.transforms import Compose
 
 class BFWFold(Dataset):
     def __init__(self, 
-    dataframe: pd.DataFrame,
-    folds: Union[int,list],
-    data_root: str = './data/bfw/',
-    transform: Optional[Compose] = None):
+                 dataframe: pd.DataFrame,
+                 folds: int|list,
+                 data_root: str = './data/bfw/',
+                 transform: Optional[callable] = None
+                ):
+        """
+        Initialize a subset of the BFW dataset for the provided folds
 
+        Provided dataframe contains columns 'fold','p1','p2' and 'label', 
+        providing what fold each pair belongs to, the paths to two images
+        and a label whether they are the same person
+
+        Method will save the part of the dataframe of the provided folds, data root and
+        transformation.
+
+        Parameters:
+            dataframe: pd.DataFrame - Dataframe having one image pair per row
+            folds: int|list - One or multiple folds to take from the dataframe
+            data_root: str - path to folder containing images
+            transform: Optional[any] - A transformation applied to any image being read
+        """
+
+        # Save this for when reading the data
         self.data_root = data_root
 
+        # For consistency, convert int to list of int
         if isinstance(folds, int):
             folds = [folds,]
 
+        # Filter dataframe for provided folds
         self.dataframe = dataframe[ dataframe['fold'].isin(folds) ]
 
-        #Uncomment to reset index such that it ranges 0,N. It now will contain holes!
+        # Uncomment to reset index such that it ranges 0,N. It now will contain holes in the index!
+        # Remember to change `BFWFold.dataframe.iloc` to `BFWFold.dataframe.loc` 
+        # in `BFWFold.__getitem__` if you do this, otherwise it has no effect on the data returned
+        
         # self.pairs_df.reset_index(inplace=True, names='global_index')
 
+        # Save transforms
         self.transform = transform
 
     def __len__(self):
+        """Give length of DataSet"""
         return len(self.dataframe)
 
     def __getitem__(self, idx):
+        """
+        Given some index, give images and label corresponding to that pair
+        TODO: Also return persion ID and sensitive attributes?
+
+        Parameters:
+            idx: ? - Index of the sample TODO: what type is idx?
+        
+        Returns:
+            img1: np.ndarray - First image of sample pair
+            img2: np.ndarray - Second image of sample pair
+            label: int - 1 if image pair is of the same person, 0 otherwise
+        """
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
@@ -54,31 +89,69 @@ class BFWFold(Dataset):
 
 class BFW():
     def __init__(self,
-    data_root: str = './data/bfw',
-    csv_file: str = './data/bfw/bfw-v0.1.5-datatable.csv',
-    transform: Optional[Compose] = None,
-    augmentation: Optional[Compose] = None):
+                 data_root: str = './data/bfw',
+                 csv_file: str = './data/bfw/bfw-v0.1.5-datatable.csv',
+                 transform: Optional[Compose] = None,
+                 augmentation: Optional[Compose] = None
+                ):
+        """
+        Set up BFW dataset as a combination of folds.
+        Method will initialize by reading the given csv file
+        Then use `trainset, testset = bfw.fold(k)` get the kth fold specified in the csv file
+
+        It will save given transform as basic processing of the data, such as .toTensor
+        If provided, augmentations will additionally applied to ONLY the train set
+
+        Parameters:
+            data_root: str - Path from which data can be found
+            csv_file: str - CSV file containing all information about the data (paths to images, labels, etc)
+            transform: Optional[Compose] - Optionally add some transformations when reading data, will be aplied to train and test data
+            augmentation: Optional[Compose] - Optionally add some agumentations, only applied to train set
+        """
+        
+        # Convert to global paths for interpretable printing
         data_root = os.path.abspath(data_root)
         csv_file = os.path.abspath(csv_file)
 
+        # Save location of data
         self.data_root = data_root
-        self.csv_file = csv_file
         
+        # Open CSV as 'data', reading of images happesn in BFWFold
         self.dataframe = pd.read_csv( csv_file )
+
+        # Save which folds are available
         self.folds = self.dataframe['fold'].unique()
+
+        # Train transformations also include augmentations
         self.train_transform = Compose([transform, augmentation])
+
+        # Test transformations
         self.test_transform = transform
 
-    def fold(self, k: int):
+    def fold(self, k: int) -> tuple[BFWFold, BFWFold]:
+        """
+        Given k, return the train and test set of the kth fold of the BFW dataset
+        It initializes these folds using the dataframe it loaded in init
+
+        Parameters:
+            k: int - What fold to return
+        
+        Returns:
+            trainSet: BFWFold - The train set, consisting of data where fold != k
+            testSet: BFWFold - The test set, consiting of data where fold == k
+        """
+        # Make sure provided fold exists
         if k not in self.folds:
             raise KeyError(f"Fold {k} not found in BFW dataset")
         
+        # Trainfolds consist of all other folds
         trainFolds = [fold for fold in self.folds if fold != k]
         trainSet = BFWFold(dataframe=self.dataframe,
                            folds=trainFolds,
                            data_root=self.data_root,
                            transform=self.train_transform)
 
+        # Testset from given fold
         testSet = BFWFold(dataframe=self.dataframe,
                           folds=[k,],
                           data_root=self.data_root,
