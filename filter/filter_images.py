9166ea26b6af154c572650b5330fd5b007210f2b
Simon <simon.stallinga@student.uva.nl>
2023-01-16 16:37:54 +0100

    added comments to functions

--- filter/filter_images.py
+++ filter/filter_images.py
@@ -17,11 +17,13 @@ def retreive_text_files(base_dir:str) -> list:
     file_name_lst = []
     walker = os.walk(base_dir)
     _ = next(walker)
+
     for (root, _, files) in walker:
-        if any('pair' in name for name in  files):
+        if any('pair' in name for name in files):
             folder_name = root.split('/')[-1]
             pair_filename = folder_name + '/' + folder_name + '_pairs.txt'
             file_name_lst.append(pair_filename)
+            
     return file_name_lst
 
 
@@ -34,6 +36,17 @@ def read_files(base_dir:str, filename_list:list, unrecognized_faces_filename:str
             base_dir: (string) base directory for the data relative to the python file
             file_name_list: (list) of directory names (strings) to itterate over and read the pair text file from
             unrecognized_faces_filename: (string) of relative path and filename of a text file with names of pictures with unrecognized faces
+
+        output:
+            info_dict: (dictionary of lists), contains the information of the filtered faces. e.g.:
+                id1: unique id of the first face
+                id2: unique id of the second face (can be the same as id1)
+                filepath1: path to the image + image name of the first image
+                filepath2: path to the image + image name of the second image
+                label: label if the images are from the same person or not (1: True, 0: False)
+            
+            optional:
+                set_filenames: (set) of filepaths + filename of all the filtered faces. To aid in the creation of embeddings
     """
     # create list of filenames of unrecognized faces for easy comparison
     with open(unrecognized_faces_filename) as unrecog_faces_file:
@@ -53,6 +66,7 @@ def read_files(base_dir:str, filename_list:list, unrecognized_faces_filename:str
 
                 striped_line = [item.strip() for item in line.split()]
 
+                # handle the case where the same person is used
                 if len(striped_line) == 3:
                     facefile_names = [f'{striped_line[0]}_{int(striped_line[1]):04}', 
                                       f'{striped_line[0]}_{int(striped_line[2]):04}']
@@ -61,7 +75,7 @@ def read_files(base_dir:str, filename_list:list, unrecognized_faces_filename:str
                     face1_filepath = base_dir[:-4] + 'data/' + filename.split('/')[0] + striped_line[0] + facefile_names[0] + '.jpg'
                     face2_filepath = base_dir[:-4] + 'data/' + filename.split('/')[0] + striped_line[0] + facefile_names[1] + '.jpg'
 
-
+                # handle the case where pictures of two different persons are used
                 elif len(striped_line) == 4:
                     facefile_names = [f'{striped_line[0]}_{int(striped_line[1]):04}', 
                                       f'{striped_line[2]}_{int(striped_line[3]):04}']
@@ -75,12 +89,15 @@ def read_files(base_dir:str, filename_list:list, unrecognized_faces_filename:str
                 else:
                     raise Exception(f'line: {line} raised an exception!')
                 
+                # filter the images
                 if all(item not in unrecognized_faces_names_lst for item in facefile_names):
+
                     info_dict['id1'].append(id1)
                     info_dict['id2'].append(id2)
                     info_dict['filepath1'].append(face1_filepath)
                     info_dict['filepath2'].append(face2_filepath)
                     info_dict['label'].append(label)
+
                     if unique_filenames:
                         set_filenames.add(face1_filepath)
                         set_filenames.add(face2_filepath)
@@ -91,6 +108,11 @@ def read_files(base_dir:str, filename_list:list, unrecognized_faces_filename:str
 
 
 def test_files(info_dict,  set_filenames = None):
+    """
+        function that tests if certain faces are NOT in the filtered set
+        The images are selected from the 'unrecognised-faces.txt' file.
+
+    """
     assert not any('m.0gy3f_0003.jpg' in item for item in set_filenames)
     assert not any('m.08zncx_0003.jpg' in item for item in set_filenames)
     assert not any('m.0r8ntwm_0002.jpg' in item for item in set_filenames)
