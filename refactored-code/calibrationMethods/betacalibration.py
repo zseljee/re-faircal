d9aaf6752fe674b0307f3457373ee0e9b9641668
12626538 <57176042+12626538@users.noreply.github.com>
2023-01-19 13:34:25 +0100

    moving calmeths and approaches to folders

--- refactored-code/calibrationMethods/betacalibration.py
+++ refactored-code/calibrationMethods/betacalibration.py
@@ -1,72 +1,10 @@
 import numpy as np
-from sklearn.isotonic import IsotonicRegression
-
-from utils import determine_edges
-from utils import bin_confidences_and_accuracies
 
 # for BetaCalibration
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.utils import indexable, column_or_1d
 from scipy.optimize import minimize_scalar
 from sklearn.linear_model import LogisticRegression
-
-import warnings
-
-
-def get_confidences(scores_cal, scores_test, ground_truth_cal, nbins, score_min=-1, score_max=1):
-    bin_edges, binning_indices = determine_edges(scores_cal, nbins, indicesQ=True, score_min=score_min,
-                                                 score_max=score_max)
-
-    print('Statistics Calibration')
-    print('Total number of scores: %d' % len(scores_cal))
-    for i in range(len(bin_edges) - 1):  # we use bin_edges since we may have not been able to form nbins
-        print('Mass in bin %d: %d' % (i, sum(binning_indices == i)))
-
-    weights, bin_confidence, _ = bin_confidences_and_accuracies(scores_cal, ground_truth_cal, bin_edges,
-                                                                binning_indices)
-
-    confidences_cal = bin_confidence[np.maximum(np.digitize(scores_cal, bin_edges, right=True) - 1, 0)]
-    confidences_test = bin_confidence[np.maximum(np.digitize(scores_test, bin_edges, right=True) - 1, 0)]
-
-    return confidences_cal, confidences_test
-
-
-def get_confidences_isotonic_regression(scores_cal, scores_test, ground_truth_cal, score_min=-1, score_max=1):
-    iso_reg = IsotonicRegression(
-        y_min=score_min,
-        y_max=score_max,
-        out_of_bounds='clip').fit(scores_cal, ground_truth_cal)
-    confidences_cal = iso_reg.predict(scores_cal)
-    if len(scores_test) > 0:
-        confidences_test = iso_reg.predict(scores_test)
-    else:
-        confidences_test = scores_test.copy()
-    return confidences_cal, confidences_test
-
-
-class IsotonicCalibration():
-    def __init__(self, scores, ground_truth, score_min=-1, score_max=1):
-        self.score_min = score_min
-        self.score_max = score_max
-        self._obj = IsotonicRegression(
-            y_min=self.score_min,
-            y_max=self.score_max,
-            out_of_bounds='clip').fit(scores, ground_truth)
-
-    def predict(self, scores):
-        return self._obj.predict(scores)
-
-
-class BinningCalibration():
-    def __init__(self, scores, ground_truth, score_min=-1, score_max=1, nbins=10):
-        self.bin_edges, self.binning_indices = determine_edges(
-            scores, nbins, indicesQ=True, score_min=score_min, score_max=score_max)
-        self.weights, self.bin_confidence, _ = bin_confidences_and_accuracies(
-            scores, ground_truth, self.bin_edges, self.binning_indices)
-
-    def predict(self, scores):
-        return self.bin_confidence[np.maximum(np.digitize(scores, self.bin_edges, right=True) - 1, 0)]
-
 
 def normalize(score, score_min=-1, score_max=1):
     return (score-score_min)/(score_max-score_min)
@@ -79,7 +17,6 @@ def normalize(score, score_min=-1, score_max=1):
 #     classifiers. AISTATS 2017.
 
 def _beta_calibration(df, y, sample_weight=None):
-    warnings.filterwarnings("ignore")
 
     df = column_or_1d(df).reshape(-1, 1)
     eps = np.finfo(df.dtype).eps
