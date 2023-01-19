44fa3b562b4ca3a26eb09c98d2257362a7903591
jgreven <jip.greven@student.uva.nl>
2023-01-19 13:34:25 +0100

    moving calmeths and approaches to folders

---
+++ refactored-code/approaches/baseline.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# from calibrationMethods import BetaCalibration
+
+def baseline(dataset, conf):
+	# cal = BetaCalibration(scores=np.zeros((100,)),
+	#                       ground_truth=np.zeros((100,)),
+	# 					  score_min=-1,
+	# 					  score_max=1)
+	return {}
