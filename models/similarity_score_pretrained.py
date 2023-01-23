f722f20431ceac2b9ab954ea91b33335025ad100
12626538 <57176042+12626538@users.noreply.github.com>
2023-01-23 23:44:21 +0100

    better similariry score

--- models/similarity_score_pretrained.py
+++ models/similarity_score_pretrained.py
@@ -7,15 +7,26 @@ import matplotlib.pyplot as plt
 
 DATA_FOLDER = os.path.abspath( './../data/' )
 
+def setup_cosine_sim(embs):
+	def cosine_sim(row):
+		v1 = np.squeeze(embs[row['path1']])
+		v2 = np.squeeze(embs[row['path2']])
+		u1 = v1/np.linalg.norm(v1)
+		u2 = v2/np.linalg.norm(v2)
+
+		return np.dot(u1, u2)
+	
+	return cosine_sim
+
 # What datasets and embeddings to combine
-dataset_names = { 'bfw', 'rfw' }
-embedding_names = { 'facenet', 'facenet-webface' }#, 'arcface'}
+dataset_names = ['bfw', 'rfw' ]
+embedding_names = ['facenet', 'facenet-webface', 'arcface']
 print("Computing similarity scores")
 print("Datasets:", dataset_names)
 print("Embeddings:", embedding_names)
 
 # Set up plto
-fig, axs = plt.subplots(len(dataset_names), len(embedding_names), figsize=(20,20), squeeze=False)
+fig, axs = plt.subplots(len(dataset_names), len(embedding_names), figsize=(20,20), squeeze=False, sharex=True, sharey='row')
 fig.suptitle("Similarities")
 
 # Iterate datasets
@@ -32,15 +43,18 @@ for dataset_name, axrow in zip(dataset_names, axs):
 		print("Embeddings from model {}, reading from {}".format(embedding_name, embedding_path))
 		with open(embedding_path, 'rb') as f:
 			embs: dict[str, np.ndarray] = pickle.load(f)
+
+			if 'full' in embs:
+				embs = embs['full']
 			
 			# Compute similarities
-			sims: pd.Series = df[ ['path1', 'path2'] ].apply( lambda row: np.dot(embs[row['path1']][0], embs[row['path2']][0]), axis=1 )
+			sims: pd.Series = df[ ['path1', 'path2'] ].apply(setup_cosine_sim(embs), axis=1 )
 			df[embedding_name] = sims
 
 			# Plot histogram
 			ax.hist( sims[df['same'] == False], color="red", label="Imposter", bins=100 )
 			ax.hist( sims[df['same'] == True], color="green", label="Genuine", bins=100 )
-			ax.set_xlim(-1,1)
+			# ax.set_xlim(-1,1)
 			ax.set_title("Dataset {}, embeddings {}".format(dataset_name, embedding_name))
 
 	# Update csv file
